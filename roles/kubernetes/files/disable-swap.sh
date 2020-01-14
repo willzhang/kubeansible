ae9f1125022672f749191f62f30e6789fbf80eaf
Your Name <you@example.com>
2020-01-15 00:08:06 +0800

    update

--- roles/kubernetes/files/disable-swap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-swapoff -a
-sysctl -w vm.swappiness=0
--- roles/kubernetes/files/kubeadm-token.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubeadm_token=`kubeadm token generate`
-sed -i "s/wise2c-breeze-token/${kubeadm_token}/g" ./kubeadm.conf

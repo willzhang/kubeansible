65484f41c76cb333bfcc58b9bb27d1f5a91be15b
Your Name <you@example.com>
2020-01-16 17:05:05 +0800

    update

--- yum-repo/build_yum_repo.sh
+++ yum-repo/build_yum_repo.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+path=`dirname $0`
+mkdir -p ${path}/rpms
+
 KUBE_VERSION=1.17.0
 DOCKER_VERSION=18.09.9-3
 
 for centos_version in centos:7.5.1804 centos:7.6.1810 centos:7.7.1908
 do
-  docker run -t --rm -v ${PWD}/rpms:/rpms -v ${PWD}/yum-repo/kubernetes.repo:/etc/yum.repos.d/kubernetes.repo ${centos_version} \
+  docker run -t --rm -v ${path}/rpms:/rpms -v ${path}/yum-repo/kubernetes.repo:/etc/yum.repos.d/kubernetes.repo ${centos_version} \
   bash -c "
   yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm &&
   yum install -y yum-utils &&
@@ -16,7 +20,7 @@ do
   yum -y install --downloadonly --downloaddir=/rpms kubernetes-cni kubectl-${KUBE_VERSION} kubelet-${KUBE_VERSION} kubeadm-${KUBE_VERSION}"
 done
 
-docker run -t --rm -v ${PWD}/rpms:/rpms -v ${PWD}/yum-repo/kubernetes.repo:/etc/yum.repos.d/kubernetes.repo centos:7.7.1908 \
+docker run -t --rm -v ${path}/rpms:/rpms -v ${path/yum-repo/kubernetes.repo:/etc/yum.repos.d/kubernetes.repo centos:7.7.1908 \
   bash -c "
   yum  install -y createrepo &&
   createrepo /rpms"
